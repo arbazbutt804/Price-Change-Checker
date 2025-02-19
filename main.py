05998867a5a9a6395feaee50966a216ecc5bcab2
arbazbutt804 <muhammad.butt@monstergroupuk.co.uk>
2025-02-19 14:54:34 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -96,6 +96,7 @@ stock_report_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTMRiRm7_GGU
 output_files = {"UK": "data_UK.csv", "EU": "data_EU.csv"}
 col1, col2, col3 = st.columns([1, 1, 1])
 with col2:
+    st.markdown("<br><br>", unsafe_allow_html=True)
     if st.button("🔍 Process Data", use_container_width=True):
         with st.spinner("Processing data"):
             df_result = process_data(price_change_urls[option], stock_report_url, output_files[option])
